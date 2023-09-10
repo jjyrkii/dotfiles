431bd86990b826700354e98889cbe2a6718d62dc
jyrki <jyrki1887@gmail.com>
2023-09-10 19:20:44 +0200

    git config and install changes

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cd /home/tim/
+
+home=(
+  ".gitconfig"
+)
+
+for i in ${home[@]}
+do
+  ln -s /home/tim/dotfiles/home/${i} /home/tim/
+done
+
 ln -s /home/tim/dotfiles/nvim /home/tim/.config/nvim/lua/user
-ln -s /home/tim/dotfiles/.gitconfig /home/tim/
