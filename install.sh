e3e280e2fc88c8fd989dc8bb5a2f6a267d2e7979
jyrki <jyrki1887@gmail.com>
2023-11-17 22:06:56 +0100

    switched to dnf and stuff

--- install.sh
+++ install.sh
@@ -3,26 +3,27 @@ DIR="$(pwd)"
 cd "$HOME" || exit
 
 echo "--- updating system ---"
-sudo rm -rf /etc/pacman.conf
-sudo ln -s "$DIR/etc/pacman.conf" /etc/
-sudo pacman -Syyu --noconfirm --needed
+sudo dnf upgrade -y
 
 packages=(
 	"zsh"
-	"base-devel"
+	"make"
+	"automake"
+	"gcc"
+	"gcc-c++"
+	"kernel-devel"
 	"zsh"
 	"nodejs"
 	"neovim"
 	"github-cli"
 	"go"
-	"google-chrome"
-	"npm"
+	"google-chrome-unstable"
 )
 
 for i in "${packages[@]}"; do
 	if ! [ -x "$(command -v "${i:?}")" ]; then
 		echo "--- installing ${i:?} ---"
-		yay -S "${i:?}" --noconfirm --needed
+		sudo dnf install "${i:?}" -y
 	else
 		echo "--- ${i:?} already installed --- "
 	fi
@@ -31,6 +32,7 @@ done
 if ! [ -x "$(command -v cargo)" ]; then
 	echo "--- installing rust ---"
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+	source "$HOME/.cargo/env"
 else
 	echo "--- rust already installed --- "
 fi
@@ -44,27 +46,26 @@ else
 	echo "--- oh-my-zsh already installed --- "
 fi
 
+
 echo "--- installing astronvim dependencies ---"
 cargo install tree-sitter-cli ripgrep bottom --locked
 go install github.com/jesseduffield/lazygit@latest
 go install github.com/dundee/gdu/v5/cmd/gdu@latest
 
+if ! [ -d "/usr/local/share/fonts/JetBrainsMono" ]; then
+	sudo mkdir "/usr/local/share/fonts"
+fi
+if ! [ -d "/usr/local/share/fonts/JetBrainsMono" ]; then
+	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+	sudo unzip JetBrainsMono.zip -d "/usr/local/share/fonts/JetBrainsMono"
+	rm -rf JetBrainsMono.zip
+fi
+
 echo "--- installing astrovim ---"
 rm -rf "$HOME/.config/nvim"
-rm -rf "$HOME/.local/share/nvim"
-rm -rf "$HOME/.local/state/nvim"
-rm -rf "$HOME/.cache/nvim"
-git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim "$HOME/.config/nvim"
 
 echo "--- creating config ---"
-etc=(
-	"pacman.conf"
-)
-for i in "${etc[@]}"; do
-	sudo rm -rf /etc/"${i:?}"
-	sudo ln -s "$DIR/etc/${i:?}" /etc/
-done
-
 home=(
 	".gitconfig"
 	".zshrc"
@@ -74,6 +75,6 @@ for i in "${home[@]}"; do
 	rm -rf "$HOME/${i:?}"
 	ln -s "$DIR/home/${i:?}" "$HOME/${i:?}"
 done
-
 echo "--- config created ---"
+
 echo "--- SETUP DONE ---"
