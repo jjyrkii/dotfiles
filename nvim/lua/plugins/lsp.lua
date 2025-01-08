550d858746ca397d25e597dea36fc578452fcc26
jjyrkii <jyrki1887@gmail.com>
2025-01-08 16:18:28 +0100

    init

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,14 @@
+return {
+  "neovim/nvim-lspconfig",
+  ---@class PluginLspOpts
+  opts = {
+    ---@diagnostic disable-next-line: undefined-doc-name
+    ---@type lspconfig.options
+    servers = {
+      -- pyright will be automatically installed with mason and loaded with lspconfig
+      bashls = {},
+      gdscript = {},
+      groovyls = {},
+    },
+  },
+}
